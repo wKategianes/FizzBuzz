3f1fd02c8453c6223eacb7639b9f791e38a0bba0
William Kategianes <wKategianes@gmail.com>
2022-08-27 02:10:08 -0700

    Added code.html
    Updated code.html
    Updated site.js
    Fixed Typos

--- js/site.js
+++ js/site.js
@@ -74,19 +74,11 @@ function displayData(fbArray){
         //grab just the tds and put them into array
         let rowCols = tableRow.querySelectorAll("td");
 
-        rowCols[0].classList.add(fbArray[index]);        
-        rowCols[0].textContent = fbArray[index];
-        
-        rowCols[1].classList.add(fbArray[index]);
-        rowCols[1].textContent = fbArray[index+1]; 
-        
-        rowCols[2].classList.add(fbArray[index]);
-        rowCols[2].textContent = fbArray[index+2];
-        
-        rowCols[3].classList.add(fbArray[index]);
+                
+        rowCols[0].textContent = fbArray[index];        
+        rowCols[1].textContent = fbArray[index+1];         
+        rowCols[2].textContent = fbArray[index+2];        
         rowCols[3].textContent = fbArray[index+3];        
-      
-        rowCols[4].classList.add(fbArray[index]);
         rowCols[4].textContent = fbArray[index+4];
         
         tableBody.appendChild(tableRow);
